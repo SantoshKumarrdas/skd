b6f0632a68e22292297b673fce3a0cc607b42234
SantoshKumarrdas <santoshy.das2004@gmail.com>
2025-08-12 19:21:21 +0530

    first add api

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'https://examrly.onrender.com',
+  baseURL: 'https://examrly.onrender.com/api',
 });
 
 // Add a request interceptor to include token
